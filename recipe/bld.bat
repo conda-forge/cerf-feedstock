ec44135c34108e69005e3f0dc5207c36b3067031
Lori A. Burns <lori.burns@gmail.com>
2023-06-01 03:36:09 -0400

    iter 4

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,8 +5,6 @@ cmake %CMAKE_ARGS% ^
   -B build ^
   -D CMAKE_BUILD_TYPE=Release ^
   -D CMAKE_INSTALL_PREFIX="%PREFIX%" ^
-  -D CMAKE_C_COMPILER=clang-cl ^
-  -D CMAKE_C_FLAGS="/EHsc %CFLAGS%" ^
   -D CMAKE_CXX_COMPILER=clang-cl ^
   -D CMAKE_CXX_FLAGS="/EHsc %CXXFLAGS%" ^
   -D CMAKE_INSTALL_LIBDIR="Library\lib" ^
@@ -14,7 +12,6 @@ cmake %CMAKE_ARGS% ^
   -D CMAKE_INSTALL_BINDIR="Library\bin" ^
   -D cerf_INSTALL_CMAKEDIR="Library\lib\cmake\cerf" ^
   -D BUILD_SHARED_LIBS=ON ^
-  -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
   -D CERF_CPP=ON ^
   -D LIB_MAN=OFF ^
   -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"
